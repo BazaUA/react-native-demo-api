db097d8886368dbdb8849023789354521f43ffac
BazaUA <vbazalickyi@gmail.com>
2018-08-14 19:19:21 +0300

    change deserialize

--- src/main/java/com/todo/list/deserializer/ToDoItemDeserializer.java
+++ src/main/java/com/todo/list/deserializer/ToDoItemDeserializer.java
@@ -14,12 +14,12 @@ public class ToDoItemDeserializer extends JsonDeserializer {
     @Override
     public Object deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-        String name = node.get("name").textValue();
+        String value = node.get("value").textValue();
         boolean isDone = node.get("done").booleanValue();
         long longDate = node.get("date").longValue();
         Date utilDate = new Date(longDate);
         ToDoItemEntity item = new ToDoItemEntity();
-        item.setName(name);
+        item.setName(value);
         item.setDone(isDone);
         item.setDate(utilDate);
         return item;
